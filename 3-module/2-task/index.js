fb495a6c5344c84c9bc8c55aa8f73a2b012668ae
cephnet <rheingold2@gmail.com>
2020-08-07 23:26:56 +0300

    Решение задач по функциям и массивам, первая часть(задачи 1-3)

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -6,4 +6,6 @@
  */
 function filterRange(arr, a, b) {
   // ваш код...
+  let filteredArr = arr.filter( (item) => item >=a && item <= b );
+  return filteredArr;
 }
--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -4,4 +4,8 @@
  */
 function camelize(str) {
   // ваш код...
+  return str.split('-').map( (element, index) => {
+    if(!index) {return element;}
+    return element = element[0].toUpperCase() + element.slice(1);
+  }).join('');
 }
