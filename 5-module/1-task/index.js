35c3b1cc1bfb000ff269d9a66ce61f0720674bc8
cephnet <rheingold2@gmail.com>
2020-08-18 22:35:11 +0300

    решение первой и второй задачи пятого модуля

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,6 @@
 function hideSelf() {
   // ваш код...
+  document.addEventListener('click', (event)=>{
+    if(event.target.classList.contains('hide-self-button')) event.target.hidden='true';
+  })
 }
