9c7398164ee72ae152b7ad0e260c87331110ae42
cephnet <rheingold2@gmail.com>
2020-08-23 20:25:57 +0300

    первая задача 6 модуля

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -11,7 +11,7 @@
      *          city: 'Petrozavodsk'
      *   },
  *
- * @constructor
+ *
  */
 /**
  * Компонент, который реализует таблицу
@@ -26,9 +26,38 @@
  *          city: 'Petrozavodsk'
  *      },ы
  *
- * @constructor
+ * 
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    this.setTh(this.elem);
+    this.elem.tBodies[0].insertAdjacentHTML('afterbegin', rows.map( (item)=>{
+      return `<tr> 
+      <td>${item.name}</td> <td>${item.age}</td> 
+      <td>${item.salary}</td> <td>${item.city}</td> 
+      </tr>`
+    }).join(''));
+    this.addButtons(this.elem);
+    this.addIvent(this.elem);
+  }
+  setTh(elem){
+    elem.insertAdjacentHTML('afterbegin', `<thead> <tr> <th>Имя</th> 
+    <th>Возраст</th> <th>Зарплата</th> <th>Город</th> <th></th> </tr> </thead>
+    <tbody> </tbody>`);
+  }
+  addButtons(elem){
+    let newTd = document.createElement('td');
+    newTd.innerHTML = `<button data-delete-row='truenpm te'> X </button>`;
+    for( let row of elem.tBodies[0].rows){
+      row.append(newTd.cloneNode(true));
+    }
+  }
+  addIvent(elem){
+    elem.addEventListener('click', (event)=>{
+      if(!event.target.dataset.deleteRow) {return};
+      let h123 = event.target.closest('tr');
+      event.target.closest('tr').remove();
+    })
   }
 }
